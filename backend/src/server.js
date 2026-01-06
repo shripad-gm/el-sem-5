453e0afbfd543afcc9c8cbdb1397c1ffcd969192
shripad-gm <shripadmaradi@gmail.com>
2026-01-07 01:40:21 +0530

    Fix: import cors in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import { ENV } from "./config/env.js";
+import cors from "cors";
 
 // routes
 import authRoutes from "./routes/auth.route.js";
