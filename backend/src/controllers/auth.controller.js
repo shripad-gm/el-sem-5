e473c46b1ddb7f96ba5537d0b0a73a16300e9aac
shripad-gm <shripadmaradi@gmail.com>
2026-01-16 00:22:32 +0530

    fix for cookie setting in logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -108,8 +108,8 @@ export const logout = (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict"
+      secure: true,
+      sameSite: "none"
     });
 
     res.status(200).json({ message: "Logout successful" });
