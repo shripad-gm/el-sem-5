8331cf67440171e7dfa791a84ebc11d38d5760c5
shripad-gm <shripadmaradi@gmail.com>
2026-01-15 12:33:47 +0530

    added cors fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   origin: [
+    "http://localhost:5173/",
     "http://localhost:3000",
     "https://civic-monitor.vercel.app"
   ],
