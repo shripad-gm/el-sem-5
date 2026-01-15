b6d88e3de089db15148356a301621a443a176350
shripad-gm <shripadmaradi@gmail.com>
2026-01-16 00:16:33 +0530

    fixed cookie issues

--- backend/src/utils/generateJwt.js
+++ backend/src/utils/generateJwt.js
@@ -8,9 +8,9 @@ const generateTokenandSetCookie = (userId, res) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: "15d"});
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production", // true in production
+        secure: true, // true in production
         maxage: 15 * 24 * 60 * 60 * 1000, // 15 days
-        sameSite: "strict"
+        sameSite: "none"
     });
 }
 
