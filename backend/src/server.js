0a18d7587dbba262bbfca9bbbc09de5bbfce54c4
shripad-gm <shripadmaradi@gmail.com>
2026-01-21 03:10:25 +0530

    added cors endpoint-2

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,6 +15,7 @@ import adminRoutes from "./routes/admin.route.js";
 const app = express();
 
 const allowedOrigins = [
+  "https://civic-sense-rvce.netlify.app",
   "https://super-crumble-3d1a8f.netlify.app",
   "http://localhost:5173",
   "http://localhost:3000",
